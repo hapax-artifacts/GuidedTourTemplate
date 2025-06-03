a326107cfe61a898a6652921f0be3ca139c8adc0
Quique <e.martinez.bueno@gmail.com>
2025-06-03 18:25:45 +0200

    cleanup

--- Packages/GuidedTourBuilder/Runntime/Scripts/AddOns/SplineFollower/SplineTrack.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.Timeline;
-
-[TrackClipType(typeof(SplinePlayableAsset))]
-[TrackBindingType(typeof(SplineFollower))]
-public class SplineTrack : TrackAsset
-{
-    // Permite editar las curvas del clip
-}
