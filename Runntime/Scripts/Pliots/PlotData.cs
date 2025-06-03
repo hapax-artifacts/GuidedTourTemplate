783a1c87c86c835252bca49833348c0eb2612ea1
Quique <e.martinez.bueno@gmail.com>
2025-06-03 19:24:54 +0200

    fixing mess

--- Runntime/Scripts/Pliots/PlotData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Plot", menuName = "Scriptable Objects/Plot")]
-public class PlotData : MonoBehaviour
-{
-    [field: SerializeField] public Segment[] segments { get; private set; } = null;
-}
